0f664dbcbdcb66acddc26b93e31db79d82419273
Faiçal Tchirou <ftchirou@gmail.com>
2015-03-05 09:58:07 +0000

    ...

--- src/test/java/com/github/ftchirou/yajl/pojo/Field.java
+++ src/test/java/com/github/ftchirou/yajl/pojo/Field.java
@@ -2,6 +2,6 @@ package com.github.ftchirou.yajl.pojo;
 
 import com.github.ftchirou.yajl.annotations.JsonTypeInfo;
 
-@JsonTypeInfo(use= JsonTypeInfo.Id.CLASS)
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS)
 public abstract class Field {
 }
--- src/test/java/com/github/ftchirou/yajl/pojo/FieldWithCustomTypeInfo.java
+++ src/test/java/com/github/ftchirou/yajl/pojo/FieldWithCustomTypeInfo.java
@@ -4,7 +4,7 @@ import com.github.ftchirou.yajl.annotations.JsonType;
 import com.github.ftchirou.yajl.annotations.JsonTypeInfo;
 import com.github.ftchirou.yajl.annotations.JsonTypes;
 
-@JsonTypeInfo(use= JsonTypeInfo.Id.CUSTOM, property="type")
+@JsonTypeInfo(use=JsonTypeInfo.Id.CUSTOM, property="type")
 @JsonTypes(
         {
             @JsonType(id="string", value=StringField2.class),
