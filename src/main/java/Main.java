6466074081d72e442708b89eee9d6acc55898202
Faiçal Tchirou <ftchirou@gmail.com>
2015-03-05 10:20:20 +0000

    add top-level API.

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.github.ftchirou.yajl.Json;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person("John", "Doe", 25, Arrays.asList("Jane", "Jill", "Helen"));
+
+        try {
+            Json.toJson(person, new File("person.json"));
+
+            Person p = Json.fromJson(new File("person.json"), Person.class);
+            System.out.println(p);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
